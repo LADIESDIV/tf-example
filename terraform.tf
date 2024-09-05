39ae3f9ee39a7cd339afaac7fc4a4690dac4f39f
Laetitia Diverchy <laeti.diverchy@gmail.com>
2024-09-05 14:20:57 +0200

    change ci

--- terraform.tf
+++ terraform.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.13.0"
+      version = "6.1.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.13.0"
+      version = "6.1.0"
     }
   }
   required_version = ">= 1.0"
